ca3576fbd22ea0619fddecf6a7037626229d031e
rrsatos <86385648+rrsatos@users.noreply.github.com>
2021-07-28 07:14:34 -0400

    Create index.spec.js

---
+++ module_6/tests/index.spec.js
@@ -0,0 +1,9 @@
+const calculator = require("./index");
+
+test("Returns 4", () => {
+    expect(calculator.plus(2,2)).toBe(4);
+});
+
+test("Returns 0", () => {
+    expect(calculator.minus(2,2)).toBe(0);
+});
